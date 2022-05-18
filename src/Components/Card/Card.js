dff0e76cebc6264224662ecafcc919f57f1f9d53
Emili Kaiman (she/her) <93098905+Ekaiman@users.noreply.github.com>
2022-05-17 18:26:06 -0600

    Create Card component

---
+++ src/Components/Card/Card.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Card = () => { 
+
+  return (
+    <div>
+      <h2>Your idea:</h2>
+      <h2>Your commercial: </h2>
+    </div>
+  )
+}
+
+
+export default Card
