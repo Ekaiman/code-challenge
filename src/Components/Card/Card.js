df726c943d2ef2f9ce683d6b7591ea7cebb25f32
Emili Kaiman (she/her) <93098905+Ekaiman@users.noreply.github.com>
2022-05-18 11:25:04 -0600

    Pass down delete prompt

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,15 +1,21 @@
-import React from "react";
+import React from 'react'
 import './Card.css'
 
-const Card = ({response, prompt, id}) => { 
-
+const Card = ({ response, prompt, deletePrompt, id }) => {
   return (
     <div className='card'>
+      <button
+        onClick={() => {
+          deletePrompt(id)
+        }}
+        id='close'
+      >
+        close
+      </button>
       <h2 className='card-text'>Your idea: {prompt} </h2>
       <h2 className='card-text'>Your commercial: {response}</h2>
     </div>
   )
 }
-
 
 export default Card
--- src/Components/CardHolder/CardHolder.js
+++ src/Components/CardHolder/CardHolder.js
@@ -2,13 +2,14 @@ import React from "react";
 import Card from '../Card/Card'
 import './CardHolder.css'
 
-const CardHolder = ({responses}) => { 
+const CardHolder = ({responses, deletePrompt}) => { 
   const cards = responses.map(response => {
    return <Card
       response={response.resp}
       prompt={response.prom}
       id={response.id}
-      key={response.id}
+     key={response.id}
+     deletePrompt={deletePrompt}
     />
   })
 
